1969cbbc9fdbe316b0db4741499f36a40a681ee2
frogmane <xzjnh8129@github.com>
2024-04-07 03:03:43 -0400

    fixed conflicting return type for virtual void pollserial::write(uint8_t)

--- pollserial/pollserial.cpp
+++ pollserial/pollserial.cpp
@@ -128,12 +128,13 @@ void pollserial::flush() {
 	rxbuffer.head = rxbuffer.tail;
 }
 
-void pollserial::write(uint8_t c) {
-#if defined ( UDR0 )
-	while (!((UCSR0A) & _BV(UDRE0)));
-	UDR0 = c;
-#else
-	while (!((UCSRA) & _BV(UDRE)));
-	UDR = c;
-#endif
+size_t pollserial::write(uint8_t c) {
+    #if defined ( UDR0 )
+	    while (!((UCSR0A) & _BV(UDRE0)));
+	    UDR0 = c;
+    #else
+	    while (!((UCSRA) & _BV(UDRE)));
+	    UDR = c;
+    #endif
+    return 1;
 }
